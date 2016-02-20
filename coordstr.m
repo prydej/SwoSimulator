a21b8125d230b2f692f2e9efa3b398592399abf5
Nuly90 <kochj6@my.erau.edu>
2016-02-19 19:11:00 -0500

    Added files via upload

---
+++ coordstr.m
@@ -0,0 +1,69 @@
+function coords = coordstr(position)
+
+%This function takes a position in seconds north and west, and converts it
+%into properly formatted coordinates.
+
+%Determine north or south
+if position(1) > 0
+    lat = 'N';
+else
+    lat = 'S';
+end
+
+%Format degrees
+degrees = floor(position(1) / 3600);
+sdegrees = num2str(degrees);
+while length(sdegrees) < 3
+    sdegrees = ['0', sdegrees];
+end
+
+%Format minutes
+minutes = floor((position(1) - degrees * 3600) / 60);
+sminutes = num2str(minutes);
+while length(sminutes) < 2
+    sminutes = ['0', sminutes];
+end
+
+%Format seconds
+sseconds = num2str(round(mod(position(1), 60)));
+while length(sseconds) < 2
+    sseconds = ['0', sseconds];
+end
+
+%Consolidate latitude
+latitude = [sdegrees, '° ' sminutes, ''' ', sseconds, '" ', lat];
+
+%Determine east or west
+if position(2) > 0
+    long = 'W';
+else
+    long = 'E';
+end
+
+%Format degrees
+degrees = floor(position(2) / 3600);
+sdegrees = num2str(degrees);
+while length(sdegrees) < 3
+    sdegrees = ['0', sdegrees];
+end
+
+%Format minutes
+minutes = floor((position(2) - degrees * 3600) / 60);
+sminutes = num2str(minutes);
+while length(sminutes) < 2
+    sminutes = ['0', sminutes];
+end
+
+%Format seconds
+sseconds = num2str(round(mod(position(2), 60)));
+while length(sseconds) < 2
+    sseconds = ['0', sseconds];
+end
+
+%Consolidate latitude
+longitude = [sdegrees, '° ' sminutes, ''' ', sseconds, '" ', long];
+
+%Put it all together
+coords = [latitude, ', ' longitude];
+
+end
